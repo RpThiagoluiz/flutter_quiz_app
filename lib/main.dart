18fd863d0a22b5c157e35e40d92da76956b72279
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2022-11-15 00:45:20 -0300

    feat: stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext ctx) {
+//State pertence ao MyApp
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = ['What is the name of...', 'What is the your god '];
+
+  void onPressAnswer(title) {
+    //setState igual react força um rerun
+    setState(() {
+      if (questionIndex <= questions.length) {
+        questionIndex += 1;
+      }
+    });
+
+    print(title);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello world!'),
+      // Scaffold responsável por criar uma pagina.
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My\'s First App'),
+        ),
+        body: Column(children: [
+          Text(questions[questionIndex]),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: () => onPressAnswer('Answer 1'),
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: () => onPressAnswer('Answer 2'),
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: () => onPressAnswer('Answer 3'),
+          )
+        ]),
+      ),
     );
   }
 }
